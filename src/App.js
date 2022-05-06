c9f9bdb0088da3214d8fca3316783aab74a5bba4
dan.nguyen <dannd496@gmail.com>
2022-05-06 12:07:06 +0700

    Buổi 1: Component, JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FunctionComponent from "./1_Component/FunctionComponent";
+import ClassComponent from "./1_Component/ClassComponent";
+import Header from "./1_Component/Header";
+import Product from "./1_Component/Product";
+import Home from "./BT_HomePage/Home";
+import Layout from "./BT_Layout/Layout";
 
+// React component
 function App() {
+  const title = "Hello React";
+
+  // JSX: Javascript XML - cho phép viết HTML trong javascript
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <h1 className="text-primary">{title}</h1>
+    //   <label htmlFor="username">Username</label>
+    //   <input type="text" id="username" />
+    //   {/* Gọi tới 1 component khác */}
+    //   <FunctionComponent></FunctionComponent>
+    //   <ClassComponent />
+    //   <Header />
+    //   <Product />
+    // </div>
+
+    // Bài tập 1: Dàn layout Home
+    // <Home />
+    // Bài tập 2: Dàn layout Bootstrap
+    <Layout />
   );
 }
 
 export default App;
+
+
+
+
+
+
